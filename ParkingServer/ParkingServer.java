598ecab86f362f3d7dee6cff7d6ac8d478b65407
prppoom <poomrapee.won@dome.tu.ac.th>
2023-11-26 14:01:30 +0700

    sprint2

--- ParkingServer/ParkingServer.java
+++ ParkingServer/ParkingServer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 
 public class ParkingServer {
     private static ArrayList<Map<Integer, Boolean>> mockUpData;
+    private static ArrayList<Map<Integer, Boolean>> inputData;
     private static String mockUpMap;
     private static boolean update;
 
@@ -38,10 +40,36 @@ public class ParkingServer {
         objectOutputStream.flush();
     }
 
+    public synchronized static ArrayList<Map<Integer, Boolean>> updateData(ArrayList<Map<Integer, Boolean>> data) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Update Data");
+        System.out.println("Index:");
+        int index = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Status:");
+        String status = scanner.nextLine();
+
+        Map<Integer, Boolean> mapToEdit = data.get(index - 1);
+        boolean newStatus = false;
+        if (status.equals("false")) {
+            newStatus = false;
+        }
+        if (status.equals("true")) {
+            newStatus = true;
+        }
+        if (mapToEdit.containsKey(index)) {
+            mapToEdit.put(index, newStatus);
+        }
+
+        update = false;
+        return data;
+    }
+
     public static void main(String[] args) {
         ParkingServer parkingServer = new ParkingServer();
 
-        ArrayList<Map<Integer, Boolean>> inputData = new ArrayList<>();
+        inputData = new ArrayList<>();
         Integer[] keys = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Boolean[] values = { true, false, false, false, true, false, true, false,
                 false, true };
@@ -64,32 +92,64 @@ public class ParkingServer {
                 + "____/";
         // mockUpMap = "";
 
-        // update = false;
+        // update = true;
 
         int portNumber = 8080;
         int statusCode;
+
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             System.out.println("Server is waiting for client connection on port " + portNumber);
             statusCode = parkingServer.getResponseStatusCode(true);
 
-            while (parkingServer.getData(inputData)) {
-                try (Socket clientSocket = serverSocket.accept();
-                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(
-                                clientSocket.getOutputStream())) {
-                    parkingServer.sendResponseStatusCode(objectOutputStream, statusCode);
-                    System.err.println("Status Code send to client: " + statusCode);
-                    parkingServer.sendData(objectOutputStream, mockUpData, mockUpMap);
-                    System.err.println("mockUpData send to client: " + mockUpData);
-                    System.err.println("mockUpMap send to client: " + mockUpMap);
-
-                } catch (IOException e) {
-                    System.err.println("Error handling client request: " + e.getMessage());
+            // Thread for updating data
+            Thread updateThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        synchronized (inputData) {
+                            inputData = updateData(inputData);
+                            boolean test = parkingServer.getData(inputData);
+                        }
+                        System.out.println("Complete update" + inputData);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+            });
+
+            // Thread for handling client connections
+            Thread serverThread = new Thread(() -> {
+                while (parkingServer.getData(inputData)) {
+                    try (Socket clientSocket = serverSocket.accept();
+                            ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                                    clientSocket.getOutputStream())) {
+
+                        parkingServer.sendResponseStatusCode(objectOutputStream, statusCode);
+                        System.err.println("Status Code send to client: " + statusCode);
+                        parkingServer.sendData(objectOutputStream, mockUpData, mockUpMap);
+                        System.err.println("mockUpData send to client: " + mockUpData);
+                        System.err.println("mockUpMap send to client: " + mockUpMap);
+
+                    } catch (IOException e) {
+                        System.err.println("Error handling client request: " + e.getMessage());
+                    }
+                }
+            });
+
+            // Start both threads
+            updateThread.start();
+            serverThread.start();
+
+            // Wait for both threads to finish
+            try {
+                updateThread.join();
+                serverThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
         } catch (IOException e) {
             System.err.println("Could not listen on port " + portNumber);
-
             e.printStackTrace();
         }
 
